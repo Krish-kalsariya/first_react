4bd0d9f75aa8829ae103bff538f844f4af9ea526
Krish Kalsariya <kalsariyakrish22@gmail.com>
2025-12-25 11:28:21 +0530

    Updated App component UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nisi expedita blanditiis minima quis repellendus labore aliquam cumque accusantium. Vero ipsa ipsam illo itaque porro neque 
+        distinctio inventore, similique eligendi.</p>
     </div>
   )
 }
